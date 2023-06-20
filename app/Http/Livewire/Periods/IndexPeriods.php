c1a5a6c5e70b5432cddbce68d8d28e1df8ad12fe
Sebastián Montero <esfredderick@gmail.com>
2023-06-20 17:18:25 -0600

    Vista índice del módulo de periodos

---
+++ app/Http/Livewire/Periods/IndexPeriods.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Periods;
+
+use Livewire\Component;
+
+class IndexPeriods extends Component
+{
+    public function render()
+    {
+        return view('livewire.periods.index-periods');
+    }
+}
