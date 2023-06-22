aac33cae39d8cbbf87d8293c0d58bc4deabad110
Sebastián Montero <esfredderick@gmail.com>
2023-06-21 23:06:11 -0600

    Inicio del módulo de procesos

---
+++ app/Http/Livewire/Precesses/IndexProcesses.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Precesses;
+
+use Livewire\Component;
+
+class IndexProcesses extends Component
+{
+    public function render()
+    {
+        return view('livewire.precesses.index-processes');
+    }
+}
